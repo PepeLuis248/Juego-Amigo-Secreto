920a2472056a3ba79ac456ad24190eb9d750edf5
PepeLuis248 <jlplanes@uolsinectis.com.ar>
2025-01-28 21:24:29 -0300

    crea /acaciones/amigo-secreto.png

---
+++ app.js
@@ -0,0 +1,47 @@
+let amigos = [];
+let i = 0;
+
+
+function agregarAmigo(){
+
+     let amigo = document.getElementById('amigo').value;
+
+     if(amigo === ''){
+
+        alert('Por favor ingresa un nombre');
+    }else{
+
+        amigos.push(amigo);
+
+        let listaAmigos = document.getElementById("listaAmigos");
+        let contenido = ''; //Variable para almacenar los elementos de la lista
+        for (let i = 0; i < amigos.length; i++){
+            contenido += `<br>${amigos[i]}</br>`; // Agregamos cada amigo como un elemento de la lista
+        }
+        listaAmigos.innerHTML = contenido;
+        
+        document.querySelector('#amigo').value = '';
+        i++
+
+        return;
+    }
+}
+function sortearAmigo(){
+         
+    if (amigos.length === 0){
+
+         alert('Por favor tenga cuidado, Array vacío ')
+      
+     }else{
+        
+         let j = Math.floor(Math.random()*amigos.length);
+         let amigoSorteado = amigos[j];
+
+         let resultado = document.getElementById("resultado");
+         resultado.innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+            
+        }
+     return;
+        
+     }
+
